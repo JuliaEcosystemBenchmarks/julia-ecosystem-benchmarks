0bc02a7cd30ec1ba6ed8a2aa30a2b8d33ac36db1
Stefan Krastanov <stefan@krastanov.org>
2025-11-22 16:15:07 -0500

    guard against corrupted git repository state

--- timetravel_setup.sh
+++ timetravel_setup.sh
@@ -4,9 +4,28 @@ export JULIA_PKG_PRECOMPILE_AUTO=0
 export JULIA_CI=true
 export JULIA_DEPOT_PATH=$PWD/depot 
 
+echo "[`date`] ##############################"
+echo "[`date`] # REGISTRY SETUP"
+echo "[`date`] ##############################"
 cd timetravel_registry
-git clone https://github.com/JuliaRegistries/General || (git -C General checkout --force master; git -C General pull --force)
+echo "[`date`] # CLONE OR CHECKOUT"
+if [ ! -d General/.git ]
+then
+    echo "[`date`] # NO REPO; PROCEED WITH CLONE"
+    git clone https://github.com/JuliaRegistries/General
+else
+    echo "[`date`] # REPO EXISTS; PROCEED WITH PRUING/GC ..."
+    rm -r General/.git/refs/remotes/origin
+    git -C General fetch -p
+    git -C General remote prune origin
+    git -C General gc --aggressive --prune=now
+    echo "[`date`] # ... AND FORCED CHECKOUT ..."
+    git -C General checkout --force master
+    echo "[`date`] # ... AND FORCED PULL"
+    git -C General pull --force
+fi
 cd General
+echo "[`date`] # CHECKOUT DATE=$JEB_REGISTRY_DATE"
 git checkout `git rev-list master -n 1 --first-parent --before="$JEB_REGISTRY_DATE"`
 git log -n1
 cd ../..
